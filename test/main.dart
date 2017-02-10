2d8f54275b8b96822a55c0ab5d27a57a9fc9f611
Patrice Chalin <pchalin@gmail.com>
2017-02-10 15:45:33 -0800

    Trim whitespace from code fragment lines

--- test/main.dart
+++ test/main.dart
@@ -8,6 +8,7 @@ import 'package:test/test.dart';
 import 'package:path/path.dart' as p;
 
 const _testDir = 'test_data';
+typedef void FileTestFunc(String filePath);
 
 // TODO: enhance tests so that we can inspect the generated error messages.
 // It might be easier to modify the updater to use an IOSink than to try to read stderr.
@@ -17,7 +18,8 @@ void main() {
 
   String _readFile(String path) => new File(path).readAsStringSync();
 
-  String _srcFileName2Path(String fileName) => p.join(_testDir, 'src', fileName);
+  String _srcFileName2Path(String fileName) =>
+      p.join(_testDir, 'src', fileName);
 
   String getSrc(String relPath) => _readFile(_srcFileName2Path(relPath));
   String getExpected(String relPath) =>
@@ -37,26 +39,41 @@ void main() {
       test(testFileName, () {
         final testFileRelativePath = p.join('no_change', testFileName);
         final originalSrc = getSrc(testFileRelativePath);
-        final updatedApiDocs = apiDocUpdater.generateUpdatedFile(_srcFileName2Path(testFileRelativePath));
+        final updatedApiDocs = apiDocUpdater
+            .generateUpdatedFile(_srcFileName2Path(testFileRelativePath));
         expect(updatedApiDocs, originalSrc);
       });
     });
   });
 
-  group('Basic code updates;', () {
+  final FileTestFunc _stdFileTest = (testFileName) {
+    test(testFileName, () {
+      final testFileRelativePath = testFileName;
+      // var originalSrc = getSrc(testFileRelativePath);
+      final updatedApiDocs = apiDocUpdater
+          .generateUpdatedFile(_srcFileName2Path(testFileRelativePath));
+      expect(updatedApiDocs, getExpected(testFileName));
+    });
+  };
+
+  group('Code updates;', () {
     final _testFileNames = [
       'no_comment_prefix.dart',
       'basic_no_region.dart',
       'basic_with_region.dart'
     ];
 
-    _testFileNames.forEach((testFileName) {
-      test(testFileName, () {
-        final testFileRelativePath = testFileName;
-        // var originalSrc = getSrc(testFileRelativePath);
-        final updatedApiDocs = apiDocUpdater.generateUpdatedFile(_srcFileName2Path(testFileRelativePath));
-        expect(updatedApiDocs, getExpected(testFileName));
-      });
+    _testFileNames.forEach(_stdFileTest);
+  });
+
+  group('Handle trailing space;', () {
+    test('test input file has expected trailing whitespace', () {
+      var fragPath = p.join(apiDocUpdater.fragmentPathPrefix,
+          'frag_with_trailing_whitespace.dart.txt');
+      var frag = _readFile(fragPath);
+      expect(frag.endsWith('\t \n'), isTrue);
     });
+
+    _stdFileTest('trim.dart');
   });
 }
